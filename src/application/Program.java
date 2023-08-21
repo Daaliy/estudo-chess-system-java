361b55d7c6da0f4dfd88df4b94ec3d79b756e246
Daaliy <trabalh0odev@gmail.com>
2023-08-20 21:46:46 -0300

    adicionados recursos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("teste");
-        System.out.println("teste");
+        Position position = new Position(3,5);
+        System.out.println(position);
     }
 }
